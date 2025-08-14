9acd8a0e1b8f52ea4f9eba88c168d7081195a0ab
Marcell Fulop <marcell.fulop@techconsulting.tech>
2025-08-14 10:42:09 -0400

    Added function

--- Sources/VideoFromURL/VideoFromURL.swift
+++ Sources/VideoFromURL/VideoFromURL.swift
@@ -1,2 +1,11 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import SwiftUI
+import AVKit
+import Foundation
+
+@ViewBuilder
+public func videoFromUrl(_ url: URL) -> some View {
+    VideoPlayer(player: AVPlayer(url: url))
+        .frame(width: 320, height: 240)
+}
